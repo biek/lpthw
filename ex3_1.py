e414e09b2f1cf811fbb041892254494cef55ba31
biek <6103119+biek@users.noreply.github.com>
2018-10-02 14:40:35 +0200

    updating contents

---
+++ ex3_1.py
@@ -0,0 +1,28 @@
+# this prints a line of text
+print "I will now count my chickens."
+# this prints a line of text and calculates a sum
+print "Hens", 25 + 30 / 6
+# this prints a line of text and calculates a sum
+print "Roosters", 100 - 25 * 3 % 4
+# this prints a line
+print "Now I will count the eggs:"
+# this calculates a sum
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# this prints a line of text
+print "Is is true that 3 + 2 < 5 - 7?"
+# this checks a sum # True or False
+print 3 + 3 < 5 - 7
+# 
+print "What is 3 + 2?", 3 + 2
+# 
+print "What is 5 - 7?", 5 -7
+# this prints a line of text
+print "Oh, that's why it's False."
+# this prints a line of text
+print "How about some more."
+# this prints a line of text and calculates a sum
+print "Is it greater?", 5 > -2
+# this 
+print "Is it greater or equal?", 5 >= -2
+# 
+print "Is it less or equal?", 5 <= -2
